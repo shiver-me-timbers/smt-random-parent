ccb023aa4ffaa2d2c8cc76e47662513a54cd314a
Karl Bennett <karl.bennett.smt@gmail.com>
2015-12-05 12:51:25 +1300

    Added comments to smt-random-things.

--- smt-random-things/src/main/java/shiver/me/timbers/data/random/RandomThings.java
+++ smt-random-things/src/main/java/shiver/me/timbers/data/random/RandomThings.java
@@ -14,16 +14,26 @@ public class RandomThings {
         return new SomeThings(ThreadLocalRandom.current());
     }
 
+    /**
+     * Randomly selects one of the supplied objects and returns it.
+     */
     @SafeVarargs
     public static <T> T someThing(T... things) {
         return things().someThing(things);
     }
 
+    /**
+     * Generates a list with a random length (max 1024) that is populated with random amounts of any of the supplied
+     * objects.
+     */
     @SafeVarargs
     public static <T> List<T> someThings(T... things) {
         return things().someThings(things);
     }
 
+    /**
+     * Generates a list with fixed a random length that is populated with random amounts of any of the supplied objects.
+     */
     @SafeVarargs
     public static <T> List<T> someThings(int length, T... things) {
         return things().someThings(length, things);
