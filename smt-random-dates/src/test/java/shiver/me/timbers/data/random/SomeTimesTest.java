5df42711e08eb9d74934cee37dc455daa61e3f02
Karl Bennett <karl.bennett.smt@gmail.com>
2015-12-05 22:37:05 +1300

    Added the copyright.

--- smt-random-dates/src/test/java/shiver/me/timbers/data/random/SomeTimesTest.java
+++ smt-random-dates/src/test/java/shiver/me/timbers/data/random/SomeTimesTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Karl Bennett
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package shiver.me.timbers.data.random;
 
 import org.junit.Before;
@@ -14,8 +30,6 @@ import static shiver.me.timbers.data.random.RandomIntegers.someInteger;
 
 public class SomeTimesTest {
 
-    private RandomDurations randomDurations;
-    private Numbers<Long> longs;
     private RandomTimeFactory<RandomTimeBuilder> randomTimeFactory;
 
     private SomeTimes times;
@@ -23,10 +37,7 @@ public class SomeTimesTest {
     @Before
     @SuppressWarnings("unchecked")
     public void setUp() {
-        randomDurations = mock(RandomDurations.class);
-        longs = mock(Numbers.class);
         randomTimeFactory = mock(RandomTimeFactory.class);
-
         times = new SomeTimes(randomTimeFactory);
     }
 
